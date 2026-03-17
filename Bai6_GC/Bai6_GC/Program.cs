77e9ebb6ac92e6e7476441c46587357bb57b4b18
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 23:12:52 +0700

    Upload bài 6

---
+++ Bai6_GC/Bai6_GC/Program.cs
@@ -0,0 +1,14 @@
+﻿        Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+        List<string> traiCay = new List<string> { "Tao", "Chuoi", "Cam", "Xoai" };
+        Console.WriteLine("Danh sach hien tai: " + string.Join(", ", traiCay));
+        Console.Write("Nhap ten trai cay can tim: ");
+        string timKiem = Console.ReadLine();
+
+        if (traiCay.Contains(timKiem))
+        {
+            Console.WriteLine($"{timKiem} co ton tai trong danh sach.");
+        }
+        else
+        {
+            Console.WriteLine($"{timKiem} khong co trong danh sach.");
+        }
