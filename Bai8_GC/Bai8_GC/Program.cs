93581b8b443f2ca8d4270b992cdcfc73d5cb7de1
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 23:21:29 +0700

    Upload bài 8

---
+++ Bai8_GC/Bai8_GC/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+Console.WriteLine("Danh sach hien tai: " + string.Join(", ", numbers));
+numbers.Reverse();
+Console.WriteLine(string.Join(", ", numbers)); 
