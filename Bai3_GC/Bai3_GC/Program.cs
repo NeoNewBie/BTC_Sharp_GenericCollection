98570fa9a0ef218f9966226ab10a8d46f3946e98
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 22:50:06 +0700

    Upload bài 3

---
+++ Bai3_GC/Bai3_GC/Program.cs
@@ -0,0 +1,24 @@
+﻿        Console.WriteLine($"Lê Anh Quốc MSV: 2415053122234");
+        List<int> danhSach = new List<int>();
+
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            danhSach.Add(int.Parse(Console.ReadLine()));
+        }
+
+        if (danhSach.Count > 0)
+        {
+            int soLonNhat = danhSach.Max();
+
+ 
+            Console.WriteLine("----------------------------");
+            Console.WriteLine($"So lon nhat trong danh sach la: {soLonNhat}");
+        }
+        else
+        {
+            Console.WriteLine("Danh sach dang trong!");
+        }
