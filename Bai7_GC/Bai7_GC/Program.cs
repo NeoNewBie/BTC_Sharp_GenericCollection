8ee523f633bd186bc0c985684a4c955cb2d77e54
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 23:16:12 +0700

    upload bài 7

---
+++ Bai7_GC/Bai7_GC/Program.cs
@@ -0,0 +1,23 @@
+﻿        Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+        List<int> numbers = new List<int>();
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap so thu {i + 1}: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+
+        numbers.Sort();
+
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Danh sach sau khi sap xep tang dan:");
+        foreach (int x in numbers)
+        {
+            Console.Write(x + " ");
+        }
+        Console.WriteLine();
+
+   
