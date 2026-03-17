4c9f49238da2bf554e4abbf6cafe08b00f5859ec
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 23:25:20 +0700

    Upload bài 9

---
+++ Bai9_GC/Bai9_GC/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+List<int> trungLap = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+Console.WriteLine(string.Join(", ", trungLap));
+List<int> duyNhat = trungLap.Distinct().ToList();
+Console.WriteLine(string.Join(", ", duyNhat));
