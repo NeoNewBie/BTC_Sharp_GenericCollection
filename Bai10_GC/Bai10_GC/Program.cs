17912b86c585d06dc3ba28071d30e6c59e4f5f76
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 23:28:47 +0700

    Upload bài 10

---
+++ Bai10_GC/Bai10_GC/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+Console.WriteLine(string.Join(", ", numbers));
+int min = numbers.Min();
+Console.WriteLine($"Gia tri nho nhat: {min}");
