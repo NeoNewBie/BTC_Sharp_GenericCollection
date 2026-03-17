673b18de3a5cbda98d0f338c6bf4ea9ee4f6fe35
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 22:34:37 +0700

    Upload bài 1

---
+++ Bai1_GC/Bai1_GC/Program.cs
@@ -0,0 +1,17 @@
+﻿
+        // 1. Tạo List<int>
+        List<int> numbers = new List<int>();
+
+        // 2. Thêm 5 số bất kỳ
+        numbers.Add(10);
+        numbers.Add(25);
+        numbers.Add(38);
+        numbers.Add(42);
+        numbers.Add(99);
+
+        // 3. In các phần tử ra màn hình
+        Console.WriteLine("Cac phan tu trong danh sach la:");
+        foreach (int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
