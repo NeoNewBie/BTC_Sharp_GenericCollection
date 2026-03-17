06343a2ebbecae4a27e5bfc80d631a1a5cb724f0
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-17 22:46:04 +0700

    Upload bài 2

---
+++ Bai2_GC/Bai2_GC/Program.cs
@@ -0,0 +1,19 @@
+﻿        
+        Console.WriteLine($"Lê Anh Quốc MSV: 2415053122234");
+        List<int> danhSachSo = new List<int>();
+
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            int giaTri = int.Parse(Console.ReadLine());
+            danhSachSo.Add(giaTri);
+        }
+
+        int tong = danhSachSo.Sum();
+
+
+        Console.WriteLine("----------------------------");
+        Console.WriteLine($"Tong cac phan tu trong danh sach la: {tong}");
