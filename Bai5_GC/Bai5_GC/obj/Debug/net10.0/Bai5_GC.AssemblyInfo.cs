6f595a8a7e06d8d04f428a955eed49d67550140f
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-18 14:26:52 +0700

    Update bài 5

--- Bai5_GC/Bai5_GC/obj/Debug/net10.0/Bai5_GC.AssemblyInfo.cs
+++ Bai5_GC/Bai5_GC/obj/Debug/net10.0/Bai5_GC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai5_GC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d185ac24a4022435927bbeace382552d588b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai5_GC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai5_GC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
