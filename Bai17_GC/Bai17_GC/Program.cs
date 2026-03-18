0ac58de4fe050c1013fe2cb69cc1413861eedf19
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-18 14:45:16 +0700

    Upload bài 17

---
+++ Bai17_GC/Bai17_GC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> { 1, 2, 2, 3, 3, 3, 4, 2 };
+
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+        foreach (int x in numbers)
+        {
+            if (counts.ContainsKey(x)) counts[x]++;
+            else counts[x] = 1;
+        }
+
+
+        int maxCount = 0;
+        int mostCommonValue = 0;
+
+        foreach (var pair in counts)
+        {
+            if (pair.Value > maxCount)
+            {
+                maxCount = pair.Value;
+                mostCommonValue = pair.Key;
+            }
+        }
+
+
+        Console.WriteLine("Danh sach: " + string.Join(", ", numbers));
+        Console.WriteLine($"So xuat hien nhieu nhat la: {mostCommonValue} ({maxCount} lan)");
+        Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+    }
+}
