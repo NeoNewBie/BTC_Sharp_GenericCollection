e03d45c72519517a8bba46db40d7ea35e0cc6754
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-18 14:29:48 +0700

    Update bài 7

--- Bai7_GC/Bai7_GC/obj/Debug/net10.0/Bai7_GC.AssemblyInfo.cs
+++ Bai7_GC/Bai7_GC/obj/Debug/net10.0/Bai7_GC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai7_GC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee523f633bd186bc0c985684a4c955cb2d77e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai7_GC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai7_GC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
