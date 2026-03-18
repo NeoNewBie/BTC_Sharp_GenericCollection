b0f395b48fb41fe05030715ee44b78b5e9c3f8c4
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-18 14:25:23 +0700

    Update bài 4

--- Bai4_GC/Bai4_GC/obj/Debug/net10.0/Bai4_GC.AssemblyInfo.cs
+++ Bai4_GC/Bai4_GC/obj/Debug/net10.0/Bai4_GC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai4_GC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b8e9db7416b0eede550c99e4ac8c7be422eb51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai4_GC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai4_GC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
