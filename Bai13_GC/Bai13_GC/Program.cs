169ac847feb4ced314f105e82932cab6818488a1
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-18 14:41:07 +0700

    Upload bài 13

---
+++ Bai13_GC/Bai13_GC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Student
+{
+    public int Id;
+    public string Name;
+
+    public Student(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Student> danhSachSV = new List<Student> {
+            new Student(1, "Nguyen Van An"),
+            new Student(2, "Tran Thi Binh"),
+            new Student(3, "Le Van Chi")
+        };
+
+        foreach (Student sv in danhSachSV)
+        {
+            // Sử dụng cấu trúc {biến, -độ_rộng} để căn lề trái
+            Console.WriteLine($"{sv.Id,-10} | {sv.Name,-20}");
+        }
+        // 2. Sử dụng thuộc tính .Count để đếm
+        int tongSoSinhVien = danhSachSV.Count;
+
+        // 3. In kết quả
+        Console.WriteLine($"==> Tong so sinh vien hien co: {tongSoSinhVien}");
+        Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+    }
+}
