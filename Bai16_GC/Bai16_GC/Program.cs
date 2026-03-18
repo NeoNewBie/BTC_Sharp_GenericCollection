60638d4c1131e0b349521914769b068e1667f5ff
LeeQ <123221920+NeoNewBie@users.noreply.github.com>
2026-03-18 14:44:27 +0700

    Upload bài 16

---
+++ Bai16_GC/Bai16_GC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap mot chuoi: ");
+        string input = Console.ReadLine();
+
+        Dictionary<char, int> demKyTu = new Dictionary<char, int>();
+
+        foreach (char c in input)
+        {
+            if (demKyTu.ContainsKey(c))
+            {
+                demKyTu[c]++;
+            }
+            else
+            {
+                demKyTu[c] = 1;
+            }
+        }
+
+        Console.WriteLine("\nKet qua dem:");
+        foreach (var pair in demKyTu)
+        {
+            Console.WriteLine($"{pair.Key} : {pair.Value}");
+        }
+        Console.WriteLine("Lê Anh Quốc MSV: 2415053122234");
+    }
+}
